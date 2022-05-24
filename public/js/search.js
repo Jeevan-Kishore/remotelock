02826c37865e2656a25261d596b1597275fda767
jeevan-kishore <kishor080@gmail.com>
2022-05-24 13:05:00 +0530

    fix(style): Add styles :lipstick:

--- public/js/search.js
+++ public/js/search.js
@@ -17,15 +17,26 @@ window.getDeviceData = async () => {
 
 window.searchHandler = async () => {
   const data = await window.getDeviceData();
+  if (!data.length) {
+    const errorMessage = `<div class='d-flex justify-content-center'>
+                             <h4 class='text-warning'>No locks found!</h4>
+                           </div>`;
+    document.getElementById('device-result').innerHTML = errorMessage;
+    return false;
+  }
   const generatedHTML = data.map((item) => {
     const listItems = Object.keys(item)
-      .map((key) => `<li>${key}: ${item[key]}</li>`)
+      .map((key) => `<li class="list-group-item">${key}: ${item[key]}</li>`)
       .join('');
-    return `<ul>${listItems}</ul>`;
+    return `<ul class="list-group col list-group-content">${listItems}</ul>`;
   });
-  document.getElementById('device-result').innerHTML = generatedHTML;
+  document.getElementById('device-result').innerHTML = generatedHTML.join('');
+  return true;
 };
 
 window.clearHandler = () => {
+  const inputField = document.getElementById('name');
+  inputField.value = '';
   document.getElementById('device-result').innerHTML = '';
+  inputField.focus();
 };
