ce87de8c44926c26eb847e876c7c9d71fe0c3b0c
jeevan-kishore <kishor080@gmail.com>
2022-05-24 10:37:11 +0530

    feat(api): Add search API :sparkles:

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
-app.use('/users', db.getUsers);
+
+/* API routes */
+app.use('/getItemByName/:name', db.getByName);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -39,6 +40,7 @@ app.use((err, req, res) => {
 });
 
 app.listen(5000, () => {
+  // eslint-disable-line
   console.log('Example app listening on port 5000');
 });
 
--- data-layer/queries.js
+++ data-layer/queries.js
@@ -1,14 +1,19 @@
 const { poolObject } = require('./setup');
 
-const getAllItems = (request, response) => {
-  poolObject.query('SELECT * FROM items', (error, results) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(results.rows);
-  });
+const getByName = (request, response) => {
+  const { name } = request.params;
+  poolObject.query(
+    'SELECT * FROM items WHERE name ilike $1',
+    [`%${name}%`],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    },
+  );
 };
 
 module.exports = {
-  getUsers: getAllItems,
+  getByName,
 };
